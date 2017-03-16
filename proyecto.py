8d236616a04cf2b7cb5aedf5139b80dce2719660
Eduardo <eduardosr130@gmail.com>
2017-03-16 11:08:11 -0500

    mejorando el codigo

--- proyecto.py
+++ proyecto.py
@@ -3,14 +3,16 @@ import os.path as path
 
 class objeto():
     def menu(self):
-        print "\n \n"
+        print "\n"
         print "BIENVENIDO \n"
         print "1. Agregar \n"
         print "2. Mostrar \n"
         print "3. Editar \n"
-        print "4. Salir \n"
+        print "4. Eliminar \n"
+        print "5. Salir \n"
         menu = int(raw_input("Que opcion quieres:"))
-        while  menu > 4 or menu == 0:
+        print "\n"
+        while  menu > 5 or menu == 0:
             menu = int(raw_input("Por favor marque como se le indica arriba:"))
         print "\n"
         return menu
@@ -41,9 +43,10 @@ class objeto():
             while estado > 1:
                 estado = int(raw_input("por favor marque como se le indico arriba necio:"))
             estado = str(estado)
-            datos = [cedula, " , ", nombre," , ",apellido," , ", edad," , ", codigo," , ", estado , "\n"]
+            datos = [cedula, " , ", nombre," , ",apellido," , ", edad," , ", codigo," , ", estado," , ", "\n"]
             archivo.writelines(datos)
             archivo.seek(finalArchivo)
+
         menu = data.menu()
         data.opcciones(menu)
         # newContent = archivo.read()
@@ -51,8 +54,34 @@ class objeto():
 
     def mostrar(self):
         archivo = open("texto2.txt", "r")
-        contenido = archivo.read()
-        print contenido
+        vector = []
+        # int(i)
+        for line in archivo.readlines():
+            base = line.split(',')
+            activo = base[5]
+            vector.append(activo)
+            numero = vector
+            cedula = base[0]
+            nombre = base[1]
+            apellido = base[2]
+            edad = base[3]
+            codigo = base[4]
+            print "Estos son datos de los empleados"
+            print "\n"
+            print "Cedula:", cedula 
+            print "\n"
+            print "Nombre:", nombre
+            print "\n"
+            print "Apellido:", apellido
+            print "\n"
+            print "Edad:", edad
+            print "\n"
+            print "Codigo:", codigo
+            print "\n"
+            print "Activo:", activo
+            print "_______________________________________________"
+
+        print "Aqui esta el vector", numero
         menu = data.menu()
         data.opcciones(menu)
 
@@ -159,6 +188,90 @@ class objeto():
         menu = data.menu()
         data.opcciones(menu)
 
+    def eliminar(self):
+        print "Escriba el numero de cedula del empleado que quiera eliminar."
+        print "\n"
+        archivo1 = open("texto2.txt", "r")
+        archivo = open("texto2.txt", "r")
+        print "Estos son los empleados que hay"
+        print "\n"
+        
+        for line in archivo1.readlines():
+            array1 = line.split(',')
+            base = array1[0]
+            cedula1 = array1[0]
+            nombre = array1[1]
+            apellido = array1[2]
+            edad = array1[3]
+            codigo = array1[4]
+            print "Cedula", cedula1 , "Nombre:", nombre , "Apellido:", apellido , "Edad:", edad , "Codigo", codigo 
+            print "\n"
+
+            
+
+        cedula = int(raw_input("Cedula:"))
+        print "\n"
+        print "_______________________________________________________________________________________________"
+        print "\n"
+        for line in archivo.readlines():
+            array = line.split(',')
+            arrayCedula = array[0]
+            arrayCedula = int(arrayCedula)
+            
+            if cedula == arrayCedula:
+                c = arrayCedula
+                archivo.close()
+                break
+            else:
+                c = 0
+
+        if c > 1:
+            archivo = open("texto2.txt", "r")
+            i = 0;
+            masEmpleados = []
+            for line in archivo.readlines():
+                array = line.split(',')
+                arrayCedula = array[0]
+                arrayCedula = int(arrayCedula)
+                if cedula == arrayCedula:
+                    base = array[0]
+                    cedula1 = array[0]
+                    nombre = array[1]
+                    apellido = array[2]
+                    edad = array[3]
+                    codigo = array1[4]
+                    print "Cedula:", cedula1 , "Nombre:", nombre , "Apellido:", apellido , "Edad:", edad , "Codigo", codigo 
+                    print "\n"
+                    print "________________________________________________________________________________________________"
+                    print "\n"
+                    print "Si deseas eliminar este empleado marque 1 si no marque 0."
+                    print "\n"
+                    negar = int(raw_input("Deseas eliminar este empleado:"))
+                    print ("\n")
+                    while negar > 1:
+                        negar = int(raw_input("Idiota marque bien:"))
+                        print "\n"
+                    
+                            
+                    if negar == 1:
+                        negar1 = int(raw_input("Esta completamente seguro que quiere eliminarlo:"))
+                        print "_____________________________________________________________"
+                        print "\n"
+                        if negar1 == 1:
+                            print "john es muy lok y estoy mejorando papito......"
+                            
+                        else:
+                            break
+                    else:
+                        break
+
+        else:
+            print "No exixte ese numero de cedula."
+            print "\n"
+
+        menu = data.menu()
+        data.opcciones(menu)
+
 
 
     def opcciones(self, menu):           
@@ -168,7 +281,7 @@ class objeto():
         if menu == 2:
             data.mostrar()
 
-        if menu == 4:
+        if menu == 5:
             repetir = 1
             while repetir != 0:
                 print "Si en verdad deseas salir marque 0 si deseas volver a las opcciones marque 1:\n"
@@ -185,6 +298,9 @@ class objeto():
         if menu == 3:
             data.editar()
 
+        if menu == 4:
+            data.eliminar()
+
 
 data = objeto()
 #menu = data.menu()
@@ -195,7 +311,8 @@ data = objeto()
 def mein():
     menu = data.menu()
     data.opcciones(menu)
-
+   
+   
 
 
             
